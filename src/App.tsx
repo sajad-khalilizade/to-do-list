21595c6d92c5413bbeb7bdb2c9339464e5c4a511
sajadkhalili <sajadkhalili73@gmail.com>
2024-10-06 20:31:32 +0330

    set dark and light for website

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 
 import HandleRoutes from './Routes';
+import ThemeToggle from './ThemeToggle';
+
 
 const App = () => {
   return (
+    <>
+    <ThemeToggle />
    <HandleRoutes />
+   </>
   );  
 }
 
